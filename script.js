859ded8a4137dd88bd0dee0555137f91d2d0043e
Kacie Cooper <kcrouch@pdx.edu>
2019-11-22 16:22:44 -0800

    updated links and fonts

--- script.js
+++ script.js
@@ -10,16 +10,6 @@ TweenMax.to(".project-section", 2.0, {
 });
 
 
-
-
-/*
-//the shadow is scaling the same as the body moves, and its
-//opacity is changing to 0.3 when the ghost goes higher
-//and we transform the origin to 50%50% so it scales from
-//the middle not at the beginning of the ellipse
-TweenMax.to(".shadow", 2.0, { scale:0.8, repeat:-1, opacity:0.1, transformOrigin:"50% 50%", yoyo:true, ease:Power1.easeInOut });
-*/
-
 // End of Your Code . Don't delete that line below!!
 });
 
