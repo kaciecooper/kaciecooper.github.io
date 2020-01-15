1a3908903078ccc6fcc71ed2ad23bd0d09d94cdb
Kacie Cooper <kcrouch@pdx.edu>
2020-01-15 13:04:13 -0800

    global link js set up

--- script.js
+++ script.js
@@ -9,6 +9,58 @@ TweenMax.to(".project-section", 2.0, {
 	ease:Power1.easeInOut 		
 });
 
+function showNav(){
+$('.navbar').html(
+
+`
+
+		
+			  <div class="dropdown-insp">
+			    <button class="dropbtn2">Inspiration
+			    </button>
+			    <div class="dropdown-content2">
+			      <a href="#"class="nav-link"></a>
+			      <a href="#"class="nav-link"></a>
+			      <a href="#"class="nav-link"></a>
+			    </div>
+			  </div> 
+	
+					
+			  <div class="dropdown">
+			    <button class="dropbtn">In Class Demos
+			    </button>
+			    <div class="dropdown-content">
+			      <a href="file:///Users/kaciecooper/Documents/GitHub/kaciecooper.github.io/css-grid.html"class="nav-link" class="hw-1-link">Css Grid</a>
+			      <a href="file:///Users/kaciecooper/Documents/GitHub/kaciecooper.github.io/media-queries.html"class="nav-link" class="hw-2-link">Media Queries</a>
+			      <a href="#"class="nav-link"></a>
+			    </div>
+				</div>
+`
+);
+
+}
+
+function addActiveClass () {
+if($('body.hw-1')) {
+	$('.hw-1-link').addClass('active')
+
+	}else if($('body.hw-2') ){
+		$('.hw-2-link').addClass('active');
+	}
+}
+
+
+function initiatize() {
+	showNav();
+	addActiveClass();
+}
+
+$(initiatize()); 
+
+
+
+
+
 
 // End of Your Code . Don't delete that line below!!
 });
