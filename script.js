14eeb31298710e378970d86ea20b9f2616873133
Kacie Cooper <kcrouch@pdx.edu>
2020-01-15 13:18:29 -0800

    added a simple nav

--- script.js
+++ script.js
@@ -10,31 +10,14 @@ TweenMax.to(".project-section", 2.0, {
 });
 
 function showNav(){
-$('.navbar').html(
+$('.globnav').html(
 
 `
-
-		
-			  <div class="dropdown-insp">
-			    <button class="dropbtn2">Inspiration
-			    </button>
-			    <div class="dropdown-content2">
-			      <a href="#"class="nav-link"></a>
-			      <a href="#"class="nav-link"></a>
-			      <a href="#"class="nav-link"></a>
-			    </div>
-			  </div> 
-	
-					
-			  <div class="dropdown">
-			    <button class="dropbtn">In Class Demos
-			    </button>
-			    <div class="dropdown-content">
-			      <a href="file:///Users/kaciecooper/Documents/GitHub/kaciecooper.github.io/css-grid.html"class="nav-link" class="hw-1-link">Css Grid</a>
-			      <a href="file:///Users/kaciecooper/Documents/GitHub/kaciecooper.github.io/media-queries.html"class="nav-link" class="hw-2-link">Media Queries</a>
-			      <a href="#"class="nav-link"></a>
-			    </div>
-				</div>
+		<ul class="flex">
+					<li><a href="index.html">Home</a></li>
+					<li><a href=“css-grid.html class=”demo-1”>Grid Demo</a></li>
+					<li><a href=“media-queries.html class=”demo-2”>Media Queries</a></li>
+				</ul>
 `
 );
 
@@ -42,10 +25,10 @@ $('.navbar').html(
 
 function addActiveClass () {
 if($('body.hw-1')) {
-	$('.hw-1-link').addClass('active')
+	$('.demo-1').addClass('active')
 
 	}else if($('body.hw-2') ){
-		$('.hw-2-link').addClass('active');
+		$('.demo-2').addClass('active');
 	}
 }
 
